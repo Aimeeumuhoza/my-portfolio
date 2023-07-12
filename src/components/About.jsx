9b0fe99bba3fc9ae6bc4a69ca6c7aa6bfda8be62
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-07-12 22:53:54 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import aboutImg from "../assets/images/about.jpeg";
+const About = () => {
+  const info = [
+    { text: "Years experience", count: "01" },
+    { text: "Completed Projects", count: "5" },
+    { text: "Companies Work", count: "01" },
+  ];
+  return (
+    <section id="about" className="py-10 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-3">
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+              Hi, I'm Aimee UMUHOZA, a software Stack Developer with a passion for creating beautiful and functional websites. I specialize in HTML, CSS, JavaScript, React.js, Node.js, and responsive design.
+
+I believe that good design is not just about aesthetics, but also about usability and accessibility. My goal is to create websites that are not only visually appealing, but also easy to use and accessible to everyone.
+he industry. I also enjoy reading books on technology and reading broadcast to expand my knowledge and gain new insights.  </p>
+              <div className="flex mt-10 items-center gap-7">
+                {info.map((content) => (
+                  <div key={content.text}>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {content.count}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">{content.text}</span>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <br />
+              
+            </div>
+          </div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-full object-cover bg-cyan-600 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
