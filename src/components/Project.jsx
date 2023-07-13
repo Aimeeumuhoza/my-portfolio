2ae287e6c1477775b4bd50ced9ef9c296c5f0652
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-07-13 19:53:29 +0200

    projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import project1 from "../assets/images/project-1.png";
-import project2 from "../assets/images/project-2.jpg";
+import project2 from "../assets/images/project-2.png";
 import project3 from "../assets/images/project-3.jpg";
 import project4 from "../assets/images/project-4.jpg";
 import project5 from "../assets/images/project-5.png";
@@ -14,35 +14,35 @@ const Project = () => {
   const projects = [
     {
       img: project1,
-      name: "Movie App",
-      github_link: "https://github.com/Sridhar-C-25",
-      live_link: "https://myreactflix.netlify.app",
+      name: "Resort hotel",
+      github_link: "https://github.com/Aimeeumuhoza/Resort-hotel",
+      
     },
     {
       img: project2,
-      name: "Job search Web App",
-      github_link: "https://github.com/Sridhar-C-25/jobsearchapp",
-      live_link: "https://myjobsearch.netlify.app",
+      name: "Chat app",
+      github_link: "https://github.com/Aimeeumuhoza/Socket",
+      
     },
-    {
-      img: project3,
-      name: "Highking",
-      github_link: "https://github.com/Sridhar-C-25/highking",
-      live_link: "https://highking01.netlify.app",
-    },
-    {
-      img: project4,
-      name: "React Nav",
-      github_link:
-        "https://github.com/Sridhar-C-25/reacttailwindnavbar-with-dropdown",
-      live_link: "https://reacttailwindnavbar.netlify.app",
-    },
-    {
-      img: project5,
-      name: "Vue Country",
-      github_link: "https://github.com/Sridhar-C-25",
-      live_link: "https://vuecountry05.netlify.app",
-    },
+    // {
+    //   img: project3,
+    //   name: "Highking",
+    //   github_link: "https://github.com/Sridhar-C-25/highking",
+    //   live_link: "https://highking01.netlify.app",
+    // },
+    // {
+    //   img: project4,
+    //   name: "React Nav",
+    //   github_link:
+    //     "https://github.com/Sridhar-C-25/reacttailwindnavbar-with-dropdown",
+    //   live_link: "https://reacttailwindnavbar.netlify.app",
+    // },
+    // {
+    //   img: project5,
+    //   name: "Vue Country",
+    //   github_link: "https://github.com/Sridhar-C-25",
+    //   live_link: "https://vuecountry05.netlify.app",
+    // },
   ];
   return (
     <section id="projects" className="py-10 text-white">
